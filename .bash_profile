4df8165fc54103235781530b6cd7d569b29827c6
Heikki Niiranen <heikki.niiranen@rohea.com>
2016-10-28 08:48:06 +0300

    Added .bash_aliases and modified bash dotfiles for vagrant use

--- .bash_profile
+++ .bash_profile
@@ -1,4 +1,5 @@
 export GITAWAREPROMPT=~/.bash/git-aware-prompt
+export LC_ALL=C
 source "${GITAWAREPROMPT}"/main.sh
 PS1="[\[\033[32m\]\h\[\033[0m\]:\[\033[35m\]\w\[\033[0m\]] \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
 
--- setup_bash.sh
+++ setup_bash.sh
@@ -4,4 +4,5 @@ DOTFILES_PATH=`pwd`
 
 # Create symlinks to bash dotfiles
 ln -s $DOTFILES_PATH'/.bash' ~/.bash
-ln -s $DOTFILES_PATH'/.bash_profile' ~/.bash_profile
+ln -s $DOTFILES_PATH'/.bash_aliases' ~/.bash_aliases
+cat $DOTFILES_PATH'/.bash_profile' >> ~/.bash_profile
